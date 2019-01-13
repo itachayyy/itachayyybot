0c10665c46d2996bad2f06b988cd4d01fc676b70
itachayyy <42098467+itachayyy@users.noreply.github.com>
2019-01-13 15:01:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,14 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
-  client.user.setGame(`on 1 servers | *help | By Itachayyy#2858 .`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
+  console.log(`Logged in as ${client.user.tag}!`);
 });
-client.on('message', message => {
-     if (message.content === ".servers") {
-     let embed = new Discord.RichEmbed()
-  .setColor("#0000FF")
-  .addField("**Server: **" , client.guilds.size)
-  message.channel.sendEmbed(embed);
-    }
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
+
  client.login("process.env.BOT_TOKEN");
