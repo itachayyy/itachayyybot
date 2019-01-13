4b6042f60b6eef2b68ea25baa9d002a451725678
itachayyy <42098467+itachayyy@users.noreply.github.com>
2019-01-13 15:16:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,25 @@
+
+  const Discord = require('discord.js');
+const { Client, Util } = require('discord.js');
+const client = new Discord.Client();
+const { PREFIX, GOOGLE_API_KEY } = require('./config');
+const prefix = '!'
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube(GOOGLE_API_KEY);
+const queue = new Map();
+const moment = require('moment');
+const fs = require("fs");
+const coolDown = new Set();
+const db = require('quick.db')
+const ms = require('ms')
+
+
+
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`*help | *inv  `,"https://www.twitch.tv/7alabygamer98")
+client.user.setGame(`#play   `,"https://www.twitch.tv/7alabygamer98")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -23,6 +42,8 @@ client.user.setGame(`*help | *inv  `,"https://www.twitch.tv/7alabygamer98")
   console.log('')
   console.log('')
 });
-     
 
-client.login("process.env.BOT_TOKEN");
+client.login(process.env.BOT_TOKEN);
+ 
+  
+  
