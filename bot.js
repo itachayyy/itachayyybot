53bc51be0fab3cdfb986e2331b39758769b2c98b
itachayyy <42098467+itachayyy@users.noreply.github.com>
2019-01-13 15:50:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,6 +43,21 @@ client.user.setGame(`#play   `,"https://www.twitch.tv/7alabygamer98")
   console.log('')
 });
 
+
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("485439687390003200").send({ embed: stewart });
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
  
   
