9fa64637eaf0e274bd4678335998639e16535c37
itachayyy <42098467+itachayyy@users.noreply.github.com>
2019-01-16 10:48:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,33 +58,7 @@ client.on('message', function(message) {
     }
 });
 
-client.on('message',message =>{
-    var prefix = "+";
-    if(message.content.startsWith('topinv')) {
-  message.guild.fetchInvites().then(i =>{
-  var invites = [];
-   
-  i.forEach(inv =>{
-    var [invs,i]=[{},null];
-     
-    if(inv.maxUses){
-        invs[inv.code] =+ inv.uses+"/"+inv.maxUses;
-    }else{
-        invs[inv.code] =+ inv.uses;
-    }
-        invites.push(`invite: ${inv.url} inviter: ${inv.inviter} \`${invs[inv.code]}\`;`);
-   
-  });
-  var embed = new Discord.RichEmbed()
-  .setColor("#000000")
-  .setDescription(`${invites.join(`\n`)+'\n\n**By:** '+message.author}`)
-  .setThumbnail("صوره بوتك هنا")
-           message.channel.send({ embed: embed });
-   
-  });
-   
-    }
-  });
+
 
 
 
