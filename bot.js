a3af7a1ec351b788f9cba208fc3ae6dacbd69dba
itachayyy <42098467+itachayyy@users.noreply.github.com>
2019-01-16 11:13:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ client.on('message', function(message) {
 
 
 client.on('message', message => { //jackeo جاكيو
-    if (message.content.startsWith("تهكير")) {
+    if (message.content.startsWith("hack")) {
   if(!message.channel.guild) return message.reply(' ');//jackeo جاكيو
       if (message.author.bot) return//jackeo جاكيو
            message.delete();//jackeo جاكيو
@@ -86,19 +86,19 @@ client.on('message', message => { //jackeo جاكيو
                m.edit({embed: new Discord.RichEmbed().setTitle(`** Hacking Done [▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓] 100%**`).setColor(0xFF0000)})
              }, 30500)//jackeo جاكيو
                 setTimeout(function() {
-               m.edit({embed: new Discord.RichEmbed().setTitle(`** ..يتم الدخول للحساب** `).setColor(0xFF0000)})
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** get into his acc... ** `).setColor(0xFF0000)})
              }, 40500)//jackeo جاكيو
                 setTimeout(function() {
-               m.edit({embed: new Discord.RichEmbed().setTitle(`** ..يتم حفض بينات الحساب** `).setColor(0xFF0000)})
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** save acc ..** `).setColor(0xFF0000)})
              }, 45500)//jackeo جاكيو
                 setTimeout(function() {
-               m.edit({embed: new Discord.RichEmbed().setTitle(`** ..يتم رفع البينات** `).setColor(0xFF0000)})
+               m.edit({embed: new Discord.RichEmbed().setTitle(`** upload acc files..** `).setColor(0xFF0000)})
              }, 50500)//jackeo جاكيو
               setTimeout(function() {
                m.delete()//jackeo جاكيو
            }, 55000)//jackeo جاكيو
              setTimeout(function() {
-               message.channel.send('** تم الاختراق  __Done Hacking__ **').then(msg => msg.delete(25000));
+               message.channel.send('**   __Done Hacking__ **').then(msg => msg.delete(25000));
            }, 60500)//jackeo جاكيو
            });//jackeo جاكيو
          }//jackeo جاكيو
