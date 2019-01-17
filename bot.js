ab0c989608b97f70c34f8cc72584e4d009d99990
itachayyy <42098467+itachayyy@users.noreply.github.com>
2019-01-17 12:50:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@
 const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
 const { PREFIX, GOOGLE_API_KEY } = require('./config');
-const prefix = '!'
+const prefix = '$'
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const youtube = new YouTube(GOOGLE_API_KEY);
