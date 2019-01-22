dce48712abc5cc3c864778684a55b1cb5b6056fc
itachayyy <42098467+itachayyy@users.noreply.github.com>
2019-01-22 13:16:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -195,6 +195,11 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
+   
+    var botCount = member.guild.members.filter(m=>m.user.bot).size
+    var memberCount = [member.guild.memberCount] - [botCount]
+    client.channels.get('536880702491197443').setName('⟫『 ${memberCount} عدد الاعضاء 』⟪');
+    client.channels.get('536880670257971220').setName('⟫『 ${botCount} عدد البوتات 』⟪');
     let channel = member.guild.channels.find('name', 'willkommen');
     let memberavatar = member.user.avatarURL
       if (!channel) return;
@@ -237,18 +242,12 @@ client.on('message', function(msg) {
 
 
 
-client.on('guildMemberAdd', member => {
-    const botCount = member.guild.members.filter(m=>m.user.bot).size
-    const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('536880702491197443').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('536880670257971220').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
-});
 
 client.on('guildMemberRemove', member => {
     const botCount = member.guild.members.filter(m=>m.user.bot).size
     const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('536880702491197443').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('536880670257971220').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
+    client.channels.get('536880702491197443').setName('⟫『 ${memberCount} عدد الاعضاء 』⟪');
+    client.channels.get('536880670257971220').setName('⟫『 ${botCount} عدد البوتات 』⟪');
 });
 
 client.on("message", message => {
